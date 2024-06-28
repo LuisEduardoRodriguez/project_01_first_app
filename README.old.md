b378557e10470f96a44f3d194ff29ff0f98a2e80
Luis Rodriguez <luiseduarddo89@gmail.com>
2024-06-28 06:04:50 -0500

    Primer Component

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# project_01_first_app
---
+++ src/Components/MyFirstComponents.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const MyFirstComponents = () => {
+  return (
+    <>
+    <div>MyfirstComponents</div>
+    <div>Hello World, Luis Eduardo Rodriguez Marin</div>
+    <div>27 de Junio de 2024</div>
+    <div>Hola Mundo2</div>
+    </>
+  )
+}
